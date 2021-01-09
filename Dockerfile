3ff5f3eafe41bd865569cb01948f1cb93c61372e
Les1ie <me@les1ie.com>
2021-01-09 12:35:25 +0800

    bugfix dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.5
+FROM python:3.6
 
 LABEL maintainer="Les1ie <me@les1ie.com>"
 
@@ -7,10 +7,10 @@ WORKDIR /app
 COPY requirements.txt .
 RUN python -m pip install -r requirements.txt -i https://pypi.douban.com/simple
 
-COPY src/ /usr/src/
-ADD docker-entrypoint.sh /docker-entrypoint.sh
-RUN chmod +x /docker-entrypoint.sh
+#COPY src/ /usr/src/
+#ADD docker-entrypoint.sh /docker-entrypoint.sh
+#RUN chmod +x /docker-entrypoint.sh
 
-WORKDIR /usr/src
-ENTRYPOINT [ "/docker-entrypoint.sh"]
+#WORKDIR /usr/src
+#ENTRYPOINT [ "/docker-entrypoint.sh"]
 CMD [ "python", "manage.py", "runserver", "0.0.0.0:8000" ]
--- your_ip/settings.py
+++ your_ip/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'rkew@5snt#f%4@e4^#*n$krh_j3pd8lt_x3=&5ji!mb%83o89o'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'your_ip.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -84,13 +81,12 @@ DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "ctf",
-        "USER": "ctf",
-        "PASSWORD": "ctf",
-        "HOST": "10.10.10.131",
-        "PORT":5432
+        "USER": "iie",
+        "PASSWORD": "iieiieiie",
+        "HOST": "cd.les1ie.com",
+        "PORT": 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -110,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -123,7 +118,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
