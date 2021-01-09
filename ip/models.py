0e5f02b15fe249d967dc0aa47894a07185ad21c8
Les1ie <me@les1ie.com>
2021-01-09 12:46:43 +0800

    fix import error

--- ip/models.py
+++ ip/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+try:
+    from django.db.models import JSONField
+except ImportError:
+    from django.contrib.postgres.fields import JSONField
+
 
 # Create your models here.
 
@@ -7,7 +12,7 @@ class MyIP(models.Model):
     """
     sample: {"ip":"1.2.3.4", "domain":"example.com"}
     """
-    ip = models.JSONField(verbose_name="addr")
+    ip = JSONField(verbose_name="addr")
 
     def __str__(self):
         return self.ip
